f249957b7d9783e271756787d4633acce92ed9c5
Jordan Christensen <jordan.c4922@gmail.com>
2019-11-08 02:56:33 +0800

    Add xc proj

---
+++ iOS-Numbers/iOS-Numbers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-Numbers
+//
+//  Created by Jordan Christensen on 11/8/19.
+//  Copyright © 2019 Mazjap Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
